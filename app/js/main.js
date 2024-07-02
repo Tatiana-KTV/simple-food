706d84e371a275a859eaf59e44555efd773d3847
Tatiana-KTV <tatiana.cosacenco@gmail.com>
2024-07-02 17:19:48 +0200

    added mixitup, fixed styles, sticky header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
 });
+
+$(document).scroll(function (e) {
+  $(window).scrollTop() > 60
+    ? $('.header-top').addClass('header-fixed')
+    : $('.header-top').removeClass('header-fixed');
+});
+
+var mixer = mixitup('.popular__content');
