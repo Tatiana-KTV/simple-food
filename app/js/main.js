c370adbf284d04b7144be94118e4083b93fa5083
Tatiana-KTV <tatiana.cosacenco@gmail.com>
2024-07-06 01:02:06 +0200

    added buttons at restorant sections, replased styles for popular and restorant sections, switched img to bk-img

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,28 @@ $(function(){
     dots: true,
     focusOnSelect: true,
     arrows: true,
-    appendArrows: '.slider-navigation',
-    prevArrow: '<div class="slider-arrow"><</div>',
+    // appendArrows: '.slider-navigation',
+    // prevArrow: '<div class="slider-arrow"></div>',
     appendDots: '.slider-navigation__dots',
-    nextArrow: '<div class="slider-arrow">></div>',
+    // nextArrow: '<div class="slider-arrow"></div>',
+
+
+  prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+  nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+  appendArrows: '.arrows-wrap'
+
   });
+
+    $('.menu__link, .logo').on('click', function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 1500);
+    });
 });
 
 
-const changeColor = document.querySelector('.menu__list');
+const changeColor = document.querySelector('.menu');
 changeColor.addEventListener('mousedown', (e) => {
   const activeClass = e.target;
   if (activeClass.tagName != 'A') return;
@@ -24,9 +37,9 @@ changeColor.addEventListener('mousedown', (e) => {
 
 
 $(document).scroll(function (e) {
-  $(window).scrollTop() > 60
-    ? $('.header-top').addClass('header-fixed')
-    : $('.header-top').removeClass('header-fixed');
+  $(window).scrollTop() > 60 
+? $('.header__top').addClass('header-fixed')
+    : $('.header__top').removeClass('header-fixed');
 });
 
 var mixer = mixitup('.popular__content');
